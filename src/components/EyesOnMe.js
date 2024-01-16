178c75bac4122cf2ebe12688c80fb20654c92d0f
Aaron Levy <aaronlevypc21@gmail.com>
2024-01-16 14:49:31 -0500

    CompleteCode

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    function handleClick() {
+    }
+    function handleFocus() {
+        console.log ('Good!')
+    }
+    function handleBlur() {
+        console.log ('Hey! Eyes on me!')
+    }
+    return(
+        <button onClick={handleClick} onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
